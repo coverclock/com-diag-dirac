31f9ba4c72e677ef96ca1c4fdb3aa56e22884b2c
Chip Overclock <coverclock@diag.com>
2025-05-09 10:37:53 -0600

    [1] Checkpoint.

--- Dirac/tst/unittest-dirac-matrix.c
+++ Dirac/tst/unittest-dirac-matrix.c
@@ -20,85 +20,86 @@ int main(void)
 
     int ii = 0;
 
-    DIRAC_OBJECT_DECL(2, 3) thing2x3a = DIRAC_OBJECT_INIT(2, 3);
-    DIRAC_ARRAY_TYPE(thing2x3_t, 2, 3);
-    thing2x3_t * matrix2x3a = DIRAC_ARRAY_POINTER(thing2x3_t, &thing2x3a);
+    DIRAC_STACK_CODE(thing2x3a, 2, 3);
 
     {
         TEST();
 
+        int rows = dirac_rows_get(thing2x3a_p);
+        int cols = dirac_columns_get(thing2x3a_p);
         int rr;
         int cc;
 
-        for (rr = 0; rr < dirac_rows_get((dirac_t *)&thing2x3a); ++rr) {
-            for (cc = 0; cc < dirac_columns_get((dirac_t *)&thing2x3a); ++cc) {
-                (*matrix2x3a)[rr][cc] = CMPLX((double)PRIMES[ii++], (double)PRIMES[ii++]);
+        for (rr = 0; rr < rows; ++rr) {
+            for (cc = 0; cc < cols; ++cc) {
+                (*thing2x3a_m)[rr][cc] = CMPLX((double)PRIMES[ii++], 0.0);
             }
         }
 
-        ASSERT(dirac_print(stdout, (dirac_t *)&thing2x3a) == (dirac_t *)&thing2x3a);
+        ASSERT(dirac_print(stdout, thing2x3a_p) == (dirac_t *)&thing2x3a);
 
         STATUS();
     }
 
-    DIRAC_OBJECT_DECL(2, 3) thing2x3b = DIRAC_OBJECT_INIT(2, 3);
-    thing2x3_t * matrix2x3b = DIRAC_ARRAY_POINTER(thing2x3_t, &thing2x3b);
+    DIRAC_STACK_CODE(thing2x3b, 2, 3);
 
     {
         TEST();
 
+        int rows = dirac_rows_get(thing2x3b_p);
+        int cols = dirac_columns_get(thing2x3b_p);
         int rr;
         int cc;
 
-        for (rr = 0; rr < dirac_rows_get((dirac_t *)&thing2x3b); ++rr) {
-            for (cc = 0; cc < dirac_columns_get((dirac_t *)&thing2x3b); ++cc) {
-                (*matrix2x3b)[rr][cc] = CMPLX((double)PRIMES[ii++], (double)PRIMES[ii++]);
+        for (rr = 0; rr < rows; ++rr) {
+            for (cc = 0; cc < cols; ++cc) {
+                (*thing2x3b_m)[rr][cc] = CMPLX((double)PRIMES[ii++], 0.0);
             }
         }
 
-        ASSERT(dirac_print(stdout, (dirac_t *)&thing2x3b) == (dirac_t *)&thing2x3b);
+        ASSERT(dirac_print(stdout, thing2x3b_p) == (dirac_t *)&thing2x3b);
 
         STATUS();
     }
 
-    DIRAC_OBJECT_DECL(3, 2) thing3x2c = DIRAC_OBJECT_INIT(3, 2);
-    DIRAC_ARRAY_TYPE(thing3x2_t, 3, 2);
-    thing3x2_t * matrix3x2c = DIRAC_ARRAY_POINTER(thing3x2_t, &thing3x2c);
+    DIRAC_STACK_CODE(thing3x2c, 3, 2);
 
     {
         TEST();
 
+        int rows = dirac_rows_get(thing3x2c_p);
+        int cols = dirac_columns_get(thing3x2c_p);
         int rr;
         int cc;
 
-        for (rr = 0; rr < dirac_rows_get((dirac_t *)&thing3x2c); ++rr) {
-            for (cc = 0; cc < dirac_columns_get((dirac_t *)&thing3x2c); ++cc) {
-                (*matrix3x2c)[rr][cc] = CMPLX((double)PRIMES[ii++], (double)PRIMES[ii++]);
+        for (rr = 0; rr < rows; ++rr) {
+            for (cc = 0; cc < cols; ++cc) {
+                (*thing3x2c_m)[rr][cc] = CMPLX((double)PRIMES[ii++], 0.0);
             }
         }
 
-        ASSERT(dirac_print(stdout, (dirac_t *)&thing3x2c) == (dirac_t *)&thing3x2c);
+        ASSERT(dirac_print(stdout, thing3x2c_p) == (dirac_t *)&thing3x2c);
 
         STATUS();
     }
 
-    DIRAC_OBJECT_DECL(5, 7) thing5x7d = DIRAC_OBJECT_INIT(5, 7);
-    DIRAC_ARRAY_TYPE(thing5x7_t, 5, 7);
-    thing5x7_t * matrix5x7d = DIRAC_ARRAY_POINTER(thing5x7_t, &thing5x7d);
+    DIRAC_STACK_CODE(thing5x7d, 5, 7);
 
     {
         TEST();
 
+        int rows = dirac_rows_get(thing5x7d_p);
+        int cols = dirac_columns_get(thing5x7d_p);
         int rr;
         int cc;
 
-        for (rr = 0; rr < dirac_rows_get((dirac_t *)&thing5x7d); ++rr) {
-            for (cc = 0; cc < dirac_columns_get((dirac_t *)&thing5x7d); ++cc) {
-                (*matrix5x7d)[rr][cc] = CMPLX((double)PRIMES[ii++], (double)PRIMES[ii++]);
+        for (rr = 0; rr < rows; ++rr) {
+            for (cc = 0; cc < cols; ++cc) {
+                (*thing5x7d_m)[rr][cc] = CMPLX((double)PRIMES[ii++], 0.0);
             }
         }
 
-        ASSERT(dirac_print(stdout, (dirac_t *)&thing5x7d) == (dirac_t *)&thing5x7d);
+        ASSERT(dirac_print(stdout, thing5x7d_p) == (dirac_t *)&thing5x7d);
 
         STATUS();
     }
@@ -106,7 +107,7 @@ int main(void)
     {
         TEST();
 
-        dirac_t * that = dirac_matrix_add((dirac_t *)&thing2x3a, (dirac_t *)&thing2x3b);
+        dirac_t * that = dirac_matrix_add(thing2x3a_p, thing2x3b_p);
         ASSERT(dirac_print(stdout, that) == that);
         ASSERT(dirac_delete(that) == (dirac_t *)0);
 
