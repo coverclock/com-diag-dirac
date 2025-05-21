2176baf35c8f0e009b999902f52e4e0571c3ef65
Chip Overclock <coverclock@diag.com>
2025-05-21 14:10:41 -0600

    [1] Added subtract unit test. PASSED

--- Dirac/tst/unittest-dirac-matrix.c
+++ Dirac/tst/unittest-dirac-matrix.c
@@ -155,6 +155,66 @@ int main(void)
     {
         TEST();
 
+        DIRAC_OBJECT_DECL(2, 3) those1 = 
+            DIRAC_OBJECT_INIT_BEGIN(2, 3)
+                { 3.0+6.0i, 5.0+9.0i, 7.0+12.0i, },
+                { 10.0+12.0i, 12.0+15.0i, 14.0+18.0i, },
+            DIRAC_OBJECT_INIT_END;
+        dirac_complex_t (*them1)[2][3] = DIRAC_MATRIX_REF(those1);
+
+        dirac_print(stdout, them1);
+
+        ASSERT(dirac_rows_get(them1) == 2);
+        ASSERT(dirac_cols_get(them1) == 3);
+
+        DIRAC_OBJECT_DECL(2, 3) those2 = 
+            DIRAC_OBJECT_INIT_BEGIN(2, 3)
+                { 0.0+0.0i, 0.0+1.0i, 0.0+2.0i, },
+                { 1.0+0.0i, 1.0+1.0i, 1.0+2.0i, },
+            DIRAC_OBJECT_INIT_END;
+        dirac_complex_t (*them2)[2][3] = DIRAC_MATRIX_REF(those2);
+
+        dirac_print(stdout, them2);
+
+        ASSERT(dirac_rows_get(them2) == 2);
+        ASSERT(dirac_cols_get(them2) == 3);
+
+        DIRAC_OBJECT_DECL(2, 3) those3 = 
+            DIRAC_OBJECT_INIT_BEGIN(2, 3)
+                { 3.0+6.0i, 5.0+8.0i, 7.0+10.0i, },
+                { 9.0+12.0i, 11.0+14.0i, 13.0+16.0i, },
+            DIRAC_OBJECT_INIT_END;
+        dirac_complex_t (*them3)[2][3] = DIRAC_MATRIX_REF(those3);
+
+        dirac_print(stdout, them3);
+
+        ASSERT(dirac_rows_get(them3) == 2);
+        ASSERT(dirac_cols_get(them3) == 3);
+
+        dirac_complex_t (*that)[2][3] = dirac_matrix_sub(them1, them2);
+        ASSERT(that != (dirac_complex_t (*)[2][3])0);
+
+        dirac_print(stdout, that);
+
+        ASSERT(dirac_rows_get(that) == 2);
+        ASSERT(dirac_cols_get(that) == 3);
+
+        int rr;
+        int cc;
+        for (rr = 0; rr < 2; ++rr) {
+            for (cc = 0; cc < 3; ++cc) {
+                ASSERT((*them3)[rr][cc] == (*that)[rr][cc]);
+            }
+        }
+
+        dirac_delete(that);
+
+        STATUS();
+    }
+
+    {
+        TEST();
+
         dirac_t * that = dirac_audit();
         ASSERT(that == (dirac_t *)0);
 
